5b3a0fb687a998be0fa60ab4fb089e8e908d7881
76824 <waldosteyn.ws@gmail.com>
2025-08-03 21:14:25 +1000

    Update live-signals.js

--- live-signals.js
+++ live-signals.js
@@ -8,14 +8,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
 const signalsRef = collection(db, "signals");
-
-// 👇 This query will always get the latest signal document based on time
 const latestSignalQuery = query(signalsRef, orderBy("time", "desc"), limit(1));
 
 const liveSignalsDiv = document.getElementById("live-signals");
 
 onSnapshot(latestSignalQuery, (snapshot) => {
-  liveSignalsDiv.innerHTML = ""; // clear previous content
+  liveSignalsDiv.innerHTML = "";
   snapshot.forEach((doc) => {
     const data = doc.data();
     const content = `
