05a600c405b2a622cd2d906ffc9a322a7c034646
76824 <waldosteyn.ws@gmail.com>
2025-08-05 21:42:40 +1000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,4 @@
-// Firebase config and initialization using global CDN (no imports)
-
+// Firebase configuration and initialization (CDN version)
 const firebaseConfig = {
   apiKey: "AIzaSyDmE1r3WUNlRTufZMaphHRiPCgBAgwsFHM",
   authDomain: "project-7459169556796997288.firebaseapp.com",
@@ -9,8 +8,6 @@ const firebaseConfig = {
   appId: "1:112926280045:web:iaece7b7d8d41265375264"
 };
 
-// Initialize Firebase
+// Initialize Firebase and Firestore using global firebase object
 firebase.initializeApp(firebaseConfig);
-
-// Firestore reference
-const db =
+const db = firebase.firestore();
